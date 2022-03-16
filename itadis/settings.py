8c0e8ba2b783d71133030feb743901404b2a85b1
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2022-03-16 13:55:50 +0600

    master

--- itadis/settings.py
+++ itadis/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'core', 
     'ckeditor',
     'djoser',
@@ -53,6 +54,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +64,29 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True   
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://domain.com",
+#     "https://api.domain.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000"
+# ]
 
 ROOT_URLCONF = 'itadis.urls'
 
